a89bfb8dd267966aae8ed85f286c0f5fd6d612b5
eekum <99074603+eekum@users.noreply.github.com>
2022-03-19 17:40:10 +0800

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,54 @@
+from ctypes import sizeof
+from enum import Enum
+
+snake = ()
+class Direction(Enum):
+    up = 0
+    down = 1
+    left = 2
+    right = 3
+
+class snake:
+    length = None
+    direction = None
+    body = None
+    block_size = None
+    color = (5,75,200)
+    bounds = None
+
+    def __init__(self, block_size, bounds):
+        self.block_size = block_size
+        self. bounds = bounds
+        self.respawn()
+
+    def respawn(self):
+        self.length = 3
+        self.body = [(20,20),(20,40),(20,60)]
+        self.direction = Direction.down
+
+    def draw(self, game, window):
+        for segment in self.body:
+            game.draw.rect(window, self.color, (segment[0],segment[1],self.block_size, self.block_size))
+
+def move(self):
+    curr_head = self.body[-1]
+    if self.direction == Direction.DOWN:
+        next_head = (curr_head[0], curr_head[1] + self.block_size)
+        self.body.append(next_head)
+    elif self.direction == Direction.UP:
+        next_head = (curr_head[0], curr_head[1] + self.block_size)
+        self.body.append(next_head)
+    elif self.direction == Direction.RIGHT:
+        next_head = (curr_head[0], self.block_size + curr_head[1])
+        self.body.append(next_head)
+    elif self.direction == Direction.LEFT:
+        next_head = (curr_head[0], self.block_size + curr_head[1])
+        self.body.append(next_head)
+
+    if self.length < len(self.body):
+        self.body.pop(0)
+
+
+
+
+
